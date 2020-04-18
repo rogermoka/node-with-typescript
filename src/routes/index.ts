31e702677d404e5984813631ece362d98be9d4a5
unknown <roger.mokarzel@gmail.com>
2020-04-18 20:36:34 -0300

    Prettier config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello aaa!' }));
-
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello aaa!' })
+);
 
 export default routes;
