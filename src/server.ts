232aa2f84f96c10c373f88a8171ea90325d64a7a
unknown <roger.mokarzel@gmail.com>
2020-04-18 19:55:00 -0300

    ESLint config

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello aaa!' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello aaa!' }));
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
