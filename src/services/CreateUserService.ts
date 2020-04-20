b9fe4491601e7dec6c2b8da2aff2d2a68f436bc0
unknown <roger.mokarzel@gmail.com>
2020-04-19 21:29:50 -0300

    criacao de registros

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { getRepository } from 'typeorm';
+import User from '../models/Users';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  async execute({ name, email, password }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const checkUserExists = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw Error('Email address already used.');
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
